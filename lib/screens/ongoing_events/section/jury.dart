55db5acda71ae3dce93b0b617936d97de17b22c7
LokeshSuryaPrakashK <lokeshsuryaprakashk@gmail.com>
2025-07-22 12:27:05 +0530

    refactor: Clean up formatting and indentation in Jury widget

--- lib/screens/ongoing_events/section/jury.dart
+++ lib/screens/ongoing_events/section/jury.dart
@@ -4,16 +4,16 @@ import 'package:e_cell_website/widgets/linear_grad_text.dart';
 import 'package:flutter/material.dart';
 
 class Jury extends StatelessWidget {
-   final OngoingEvent event;
+  final OngoingEvent event;
   final bool isMobile;
   final bool isTablet;
   final double screenWidth;
-  const Jury({
-    required this.event,
-    required this.isMobile,
-    required this.isTablet,
-    required this.screenWidth,
-    super.key});
+  const Jury(
+      {required this.event,
+      required this.isMobile,
+      required this.isTablet,
+      required this.screenWidth,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,79 +29,73 @@ class Jury extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 22),
-       
-       SizedBox(
-        height: 260,
-         child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 6, 
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: _jurycard(),
-            );
-          },),
-       ),
-
+        SizedBox(
+          height: 260,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: _jurycard(),
+              );
+            },
+          ),
+        ),
         const SizedBox(height: 20),
       ],
     );
   }
 }
 
-Widget _jurycard(){
+Widget _jurycard() {
   return Container(
-    height: 220,
-    width: 180,
-    decoration: BoxDecoration(
-      color: const Color.fromARGB(255, 41, 40, 40),
-      borderRadius: BorderRadius.circular(20),
-      border: Border.all(
-        color: Colors.white,
-        width: 1,
+      height: 220,
+      width: 180,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 41, 40, 40),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.white,
+          width: 1,
+        ),
       ),
-      
-    ),
-    child:Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children:[
-        Container(
-          height: 150,
-          width: 180,
-          decoration: BoxDecoration(
-            color:  Colors.black,
-            borderRadius: BorderRadius.circular(20),
-            //white border only on the bottom
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.white,
-                width: 1,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              width: 180,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(20),
+                //white border only on the bottom
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.white,
+                    width: 1,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        SizedBox(height: 0),
-        ListTile(
-          title: Text(
-            'Jury Name',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-              fontWeight: FontWeight.bold,
+            SizedBox(height: 0),
+            ListTile(
+              title: Text(
+                'Jury Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                'Designation',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 10,
+                ),
+              ),
             ),
-          ),
-          subtitle: Text(
-            'Designation',
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 10,
-            ),
-          ),
-          
-        ),
-      ]
-    )
-
-  );
+          ]));
 }
